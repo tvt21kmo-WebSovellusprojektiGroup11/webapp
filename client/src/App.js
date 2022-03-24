c001c4cd045d46cad17d173276c30e90756d9bc4
Immu Saarijärvi <immu.saarijarvi@gmail.com>
2022-03-24 16:56:08 +0200

    react-app pohja luotu

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './components/Home'
+import SignupView from './components/SignupView'
+
+function App() {
+  return (
+    <div>
+      <h1>Food orderin app</h1>
+      <Home/>
+      <SignupView/>
+    </div>
+  );
+}
+
+export default App;
