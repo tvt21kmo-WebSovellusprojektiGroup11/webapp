290802f787d6eb69b408f9d5db8f70e87a3a1dc8
Immu Saarijärvi <immu.saarijarvi@gmail.com>
2022-03-25 18:15:51 +0200

    uuden käyttäjän luomisen frontend osuus valmis, backend puuttuu vielä kokonaan

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import Home from './components/Home'
-import SignupView from './components/SignupView'
+import Home from './components/Home';
+import SignupView from './components/SignupView';
+import LoginView from './components/LoginView';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Food orderin app</h1>
-      <Home/>
-      <SignupView/>
+      <h1>Ruokatilaussovellus</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/login" element={ <LoginView/> } />
+          <Route path="/signup" element={ <SignupView/> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
