38b937a6c3e804453b00948dd80e002dfc42f6ec
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-04-04 22:53:00 +0300

    Yhteys kantaan nyt myös suljetaan

--- server/routes/tuote.js
+++ server/routes/tuote.js
@@ -10,24 +10,24 @@ router.post('/uusi', (req, res) => {
         req.body.hinta,
         req.body.kuva
     ]
-    var sqlKasky = 'INSERT INTO Tuote ( nimi, kuvau, kategoria, hinta, kuva ) VALUES ?';
+    var sqlKasky = 'INSERT INTO Tuote ( nimi, kuvaus, kategoria, hinta, kuva ) VALUES ?';
     pool.getConnection(async function (err, connection) {
         if (err) throw err;
-        connection.promise().query(sqlKasky[[lisattu_tuote]], function (err) {
-            if (err) throw err;
-        }).then(
-            connection.destroy()
-        ).then(
+        connection.promise().query(sqlKasky, [[lisattu_tuote]]).then(
             res.status(201).json({ status: "created" })
         )
     })
 })
+
 router.get('/', (req, res) => {
     pool.getConnection(async function (err, connection) {
         if (err) throw err;
-        connection.promise().query('SELECT * FROM Tuote').then(rivit => {
+        connection.promise().query('SELECT * FROM Tuote').then(
+            connection.end()
+        ).then(rivit => {
             res.send(rivit[0]);
         })
     })
 })
+
 module.exports = router;
