b8da5d1e70ae71d7872da38f94805a0d2ecd0ceb
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-04-08 02:11:05 +0300

    Uudet kanta muutokset käyttöön

--- server/routes/tuote.js
+++ server/routes/tuote.js
@@ -1,20 +1,27 @@
 const pool = require('../db_handler')();
 const express = require('express');
+const passport = require('passport');
 var router = express.Router();
 
-router.post('/uusi', (req, res) => {
-    const lisattu_tuote = [
+router.post('/uusi', passport.authenticate('jwt', { session: false }), (req, res) => {
+    var lisatty_tuote = [
         req.body.nimi,
         req.body.kuvaus,
         req.body.kategoria,
         req.body.hinta,
-        req.body.kuva
+        req.body.kuva,
     ]
-    var sqlKasky = 'INSERT INTO Tuote ( nimi, kuvaus, kategoria, hinta, kuva ) VALUES ?';
+    var sqlKasky = 'INSERT INTO Tuote ( Nimi, Kuvaus, Kategoria, Hinta, Kuva, Valmistaja ) VALUES ?';
+    var haeRavintola = 'SELECT idRavintola FROM RAVINTOLA WHERE Omistaja = ?'
     pool.getConnection(async function (err, connection) {
         if (err) throw err;
-        connection.promise().query(sqlKasky, [[lisattu_tuote]]).then(
-            res.status(201).json({ status: "created" })
+        connection.promise().query(haeRavintola, [[req.user.kayttaja.idKayttaja]]).then(
+            rivit => {
+                lisatty_tuote.push(rivit[0][0]);
+                connection.promise().query(sqlKasky, [[lisatty_tuote]]).then(
+                    res.status(201).json({ status: "created" })
+                )
+            }
         )
     })
 })
