578932b50fa3bff9628b45d0572b02b6de51c1bc
Immu Saarijärvi <immu.saarijarvi@gmail.com>
2022-04-09 13:34:22 +0300

    Ravintolan tyyppi muutetaan lyhennetystä versiosta täysmittaiseksi UI:ssa

--- client/src/components/RestaurantItems.js
+++ client/src/components/RestaurantItems.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
 export default function RestautantItems (props) {
+
+  let tyyppi = null;
+  switch(props.Tyyppi){
+    case "b": 
+      tyyppi = "Buffet";
+      break;
+
+    case "ff":
+      tyyppi = "Fast food";  
+      break;
+
+    case "fc": 
+    tyyppi = "Fast casual";  
+      break;
+    
+    case "cd":
+      tyyppi = "Casual dining";  
+      break;
+    
+    case "fd":
+      tyyppi = "Fine dining";  
+      break;  
+  }
+
+  if(props.Tyyppi)
   return (
     <div className="restaurantItem">
     <div><h3>{ props.Nimi }</h3></div>
     <img src={ props.Kuva } />
-    <div>Aukioloajat: {props.Aukioloaika}</div>
-    <div>{ props.Tyyppi }</div>
+    <div>Aukioloaika: {props.Aukioloaika}</div>
+    <div>{ tyyppi }</div>
     </div>);   
 }
