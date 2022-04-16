cc336ac7eead5d06ee8a958206eac74e3ab57226
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-04-17 01:47:04 +0300

    Tuotteen ulkonäky viimeistelylomakkeessa

---
+++ client/src/components/KarryItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function MenuItem(props) {
+    var tyyppi;
+    switch (props.Kategoria) {
+        case ("p"):
+            tyyppi = "pääruoka"
+        case ("j"):
+            tyyppi = "jälkiruoka"
+        case ("a"):
+            tyyppi = "aamupala"
+    }
+    return (
+        <div className="restaurantItem">
+            <div><h3>{props.Nimi}</h3></div>
+            <img src={props.Kuva} />
+            <div>Hinta: {props.Hinta}</div>
+            <div>{tyyppi}</div>
+            <div>{props.Kuvaus}</div>
+        </div>
+    );
+}
